7f3389456bfbada03f9d46f4aa1b7254fd2ffe84
Pedro Tavares de Carvalho <ptcar@pteco.dev>
2021-05-10 00:33:10 -0300

    fix(plugin): changes alias declaration

--- inspr.plugin.zsh
+++ inspr.plugin.zsh
@@ -3,26 +3,26 @@ if [ $commands[inspr] ]
 then
 	source <(inspr completion zsh)
 fi
-alias i inspr
+alias i=inspr
 
-alias iga inspr get apps
-alias ida inspr describe apps
-alias idela inspr delete apps
+alias iga=inspr get apps
+alias ida=inspr describe apps
+alias idela=inspr delete apps
 
-alias igt inspr get ctypes
-alias idt inspr describe ctypes
-alias idelt inspr delete ctypes
+alias igt=inspr get ctypes
+alias idt=inspr describe ctypes
+alias idelt=inspr delete ctypes
 
-alias igc inspr get channels
-alias idc inspr describe channels
-alias idelc inspr delete channels
+alias igc=inspr get channels
+alias idc=inspr describe channels
+alias idelc=inspr delete channels
 
-alias iga inspr get apps
-alias ida inspr describe apps
-alias idela inspr delete apps
+alias iga=inspr get apps
+alias ida=inspr describe apps
+alias idela=inspr delete apps
 
-alias iaf inspr apply -f
-alias iad inspr apply -k
+alias iaf=inspr apply -f
+alias iad=inspr apply -k
 
-alias ico inspr config
-alias icl inspr config list
+alias ico=inspr config
+alias icl=inspr config list
